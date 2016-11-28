92c12f3d78507f44e7c50009a55cb1dd0ebdf5fb
arpigh <arpighazaryan99@mail.ru>
2016-11-28 18:32:59 +0300

    Add files via upload

--- prog 6.py
+++ prog 6.py
@@ -1,14 +1,7 @@
 
-s = 'abracadabra'
+s = 'Arpi'
 print(s)
 print(s[1:11])
 print(s[2:11])
 print(s[3:11])
 print(s[4:11])
-print(s[5:11])
-print(s[6:11])
-print(s[7:11])
-print(s[8:11])
-print(s[9:11])
-print(s[10:11])
-
