707ef6af160efd102a8984934ca9aaccb1175b58
arpigh <arpighazaryan99@mail.ru>
2017-05-30 10:58:06 +0300

    Add files via upload

---
+++ examm/exam.py
@@ -0,0 +1,24 @@
+#задание 1
+import re
+f_words = open('D:\\s.txt', 'r', encoding='utf-8').read() #читаем статью из файла 
+ 
+func_words = f_words.strip().split()
+
+bigrams = [] # будем собирать пары слов и далее их обрабатывать
+for i in range(1, len(func_words) - 1):
+    bigrams.append(' '.join([func_words[i - 1], func_words[i]]))
+
+name_fam = [b for b in bigrams if re.search('^[А-Я]\. [А-Я][^\s\d\.\,]+', b)]
+print(name_fam)
+
+#задание 2
+
+name_fam1 = [b for b in bigrams if re.search('^[А-Я][а-я]+ [А-Я][а-я]', b)]
+
+bigramss = [] # будем собирать для формала А. Ф. Иванов по 3 слова и далее их обрабатывать
+for i in range(2, len(func_words) - 1):
+    bigramss.append(' '.join([func_words[i - 2],func_words[i - 1], func_words[i]]))
+
+name_fam2 = [b for b in bigramss if re.search('^[А-Я]\. [А-Я]\. [А-Я][^\s\d\.\,]+', b)]
+print(name_fam1)
+print(name_fam2)
